15daa0c968651fa024ad92b7785d955576808705
Hansie <63493237+hansie23@users.noreply.github.com>
2022-04-26 00:37:06 +0100

    Add files via upload

---
+++ first_scraper.py
@@ -0,0 +1,47 @@
+from requests_html import HTMLSession
+from bs4 import BeautifulSoup as bs
+import numpy as np
+import pandas as pd
+
+# url = 'https://uk.indeed.com/jobs?q=Graduate&start=00'
+
+job_title = []
+companies = []
+location = []
+salary = []
+
+pages = pd.Series(range(0,760,10))
+
+for a in pages:
+    url = f'https://uk.indeed.com/jobs?q=Graduate&start={a}'
+    s = HTMLSession()
+    r = s.get(url)
+    soup = bs(r.text, 'html.parser')
+    
+    containers = soup.find_all('div',{'class':'job_seen_beacon'})
+    
+    for container in containers:
+        job = container.find('h2', {'class':'jobTitle'}).text
+        company = container.find('span', {'class':'companyName'}).text
+        loc = container.find('div', {'class':'companyLocation'}).text
+        
+        job_title.append(job)
+        companies.append(company)
+        location.append(loc)
+            
+        try:
+            sal = container.find('div', {'class':'salary-snippet'}).text
+            salary.append(sal)
+        except AttributeError:
+            salary.append('N/A')
+
+data_dict = {
+    'job_titles':job_title,
+    'companies':companies,
+    'location':location,
+    'salary':salary
+    }
+
+df = pd.DataFrame(data_dict)
+
+df.to_csv('indeed_jobs.csv', index=False, header=True)  # exported to .csv file
